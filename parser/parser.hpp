b96ab76e646d4250ae7871cab35cb8afa613a6c7
mo-san99 <moboustta6@gmail.com>
2021-11-02 18:03:14 +0100

    start parsing config file

--- parser/parser.hpp
+++ parser/parser.hpp
@@ -2,7 +2,29 @@
 // Created by Mohammad  Boustta on 11/2/21.
 //
 
-#ifndef WEBSERV_PARSER_HPP
-#define WEBSERV_PARSER_HPP
+#ifndef __PARSER_HPP__
+#define __PARSER_HPP__
 
-#endif //WEBSERV_PARSER_HPP
+class eGlobalDirectives {
+public:
+        static const int PORT = 0;
+        static const int HOST = 1;
+        static const int SERVER_NAME = 2;
+        static const int ERROR_PAGE = 3;
+        static const int MAX_FILE_SIZE = 4;
+        static const int TIME_OUT = 5;
+        static const int LOCATION = 6;
+        static const int INVALID = 7;
+};
+
+class eLocationDirectives : public eGlobalDirectives
+{
+public:
+    static const int  LOC_ROOT = 9;
+    static const int  LOC_ALLOWED_METHODS = 10;
+    static const int  LOC_INDEX = 11;
+    static const int  LOC_AUTO_INDEX = 12;
+    static const int  LOC_AUTH_BASIC = 13;
+};
+
+#endif // __PARSER_HPP__
