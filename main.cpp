915165bda7c96e3197253f63ee6c9ecf38ccb1cf
Khalid Eddib <keddib@Khalids-MacBook-Pro.local>
2021-11-08 19:54:17 +0100

    handle multiple blocks of servers and also location blocks

--- main.cpp
+++ main.cpp
@@ -3,11 +3,8 @@
 #include <map>
 #include <sstream>
 #include <algorithm>
-#include <stack>
 #include <fstream>
-#include <sstream>
 #include <iostream>
-#include <map>
 #include "parser/parser.hpp"
 #include <array>
 
@@ -19,21 +16,32 @@ std::ostream &operator<<(std::ostream &os, std::vector<std::string> const &vec) 
     return os;
 }
 
-std::ostream &operator<<(std::ostream &os, std::vector<Server*> const &vec) {
+std::ostream &operator<<(std::ostream &os, std::vector<Server *> const &vec) {
     os << "[" << std::endl;
     for (int i = 0; i < vec.size(); ++i) {
-        os << "\tport: " << vec[i]->_port << std::endl;
-        os << "\thost: " << vec[i]->_host << std::endl;
-        os << "\tserver_name: " << vec[i]->_server_name << std::endl;
-        os << "\terror_page: " << vec[i]->_error_page << std::endl;
-        os << "\tmax_file_size: " << vec[i]->_max_file_size << std::endl;
-        os << "\ttime_out: " << vec[i]->_time_out << std::endl;
-        os << "\t\tlocation: " << std::endl;
-        os << " \t\tlocation__root: " << vec[i]->loc._root << std::endl;
-        os << " \t\tlocation__allowed_method: " << vec[i]->loc._allowed_method << std::endl;
-        os << " \t\tlocation__index: " << vec[i]->loc._index << std::endl;
-        os << " \t\tlocation__auto_index: " << vec[i]->loc._auto_index << std::endl;
-        os << " \t\tlocation__auth_basic: " << vec[i]->loc._auth_basic << std::endl;
+        std::cout << "--------------------- Server " << i + 1 << " ---------------------" << std::endl;
+
+        os << "port: " << vec[i]->_port << std::endl;
+        os << "host: " << vec[i]->_host << std::endl;
+        os << "server_name: " << vec[i]->_server_name << std::endl;
+        os << "error_page: " << vec[i]->_error_page << std::endl;
+        os << "max_file_size: " << vec[i]->_max_file_size << std::endl;
+        os << "time_out: " << vec[i]->_time_out << std::endl;
+
+        for (size_t j = 0; j < vec[i]->_location.size(); j++) {
+            os << "\tlocation" << std::endl;
+            os << "\t\troot: " << vec[i]->_location[j]->_root << std::endl;
+            os << "\t\tallowed_method: ";
+            for (std::set<std::string>::iterator ut = vec[i]->_location[j]->_allowed_method.begin();
+                 ut != vec[i]->_location[j]->_allowed_method.end(); ++ut)
+                os << *ut << " ";
+            os << std::endl;
+            os << "\t\tindex: " << vec[i]->_location[j]->_index << std::endl;
+            os << "\t\tauto_index: " << vec[i]->_location[j]->_auto_index << std::endl;
+            os << "\t\tauth_basic: " << vec[i]->_location[j]->_auth_basic << std::endl;
+        }
+
+        std::cout << std::endl;
     }
     os << ']' << std::endl;
     return os;
@@ -45,24 +53,29 @@ void exitError(std::string const &error) {
 }
 
 int getDirective(std::string const &token) {
-    static const std::array<std::pair<std::string, int>, 12> globalDirectives = {
-            std::make_pair("port", eGlobalDirectives::PORT),
-            std::make_pair("host", eGlobalDirectives::HOST),
-            std::make_pair("server_name", eGlobalDirectives::SERVER_NAME),
-            std::make_pair("error_page", eGlobalDirectives::ERROR_PAGE),
-            std::make_pair("max_file_size", eGlobalDirectives::MAX_FILE_SIZE),
-            std::make_pair("time_out", eGlobalDirectives::TIME_OUT),
-            std::make_pair("location", eGlobalDirectives::LOCATION),
-            std::make_pair("root", eLocationDirectives::LOC_ROOT),
-            std::make_pair("allowed_method", eLocationDirectives::LOC_ALLOWED_METHODS),
-            std::make_pair("index", eLocationDirectives::LOC_INDEX),
-            std::make_pair("auto_index", eLocationDirectives::LOC_AUTO_INDEX),
-            std::make_pair("auth_basic", eLocationDirectives::LOC_AUTH_BASIC),
+    static const std::array<std::pair<std::string, int>, 16> globalDirectives = {
+            std::make_pair("port", Directives::PORT),
+            std::make_pair("host", Directives::HOST),
+            std::make_pair("server_name", Directives::SERVER_NAME),
+            std::make_pair("error_page", Directives::ERROR_PAGE),
+            std::make_pair("max_file_size", Directives::MAX_FILE_SIZE),
+            std::make_pair("time_out", Directives::TIME_OUT),
+            std::make_pair("location", Directives::LOCATION),
+            std::make_pair("root", Directives::ROOT),
+            std::make_pair("allowed_method", Directives::ALLOWED_METHODS),
+            std::make_pair("index", Directives::INDEX),
+            std::make_pair("cgi", Directives::CGI),
+            std::make_pair("redirect", Directives::REDIRECT),
+            std::make_pair("upload_pass", Directives::UPLOAD),
+            std::make_pair("auto_index", Directives::AUTO_INDEX),
+            std::make_pair("auth_basic", Directives::AUTH_BASIC),
+            std::make_pair("]", Directives::LOCATION_END),
     };
 
     for (int i = 0; i < globalDirectives.size(); ++i)
-        if (token == globalDirectives[i].first) return globalDirectives[i].second;
-    return eGlobalDirectives::INVALID;
+        if (token == globalDirectives[i].first)
+            return globalDirectives[i].second;
+    return e_error::INVALID_DIRECTIVE;
 }
 
 std::vector<std::string> split(std::string const &line, char del = ' ') {
@@ -70,106 +83,146 @@ std::vector<std::string> split(std::string const &line, char del = ' ') {
     std::stringstream ss(line);
     std::string tmp;
     while (std::getline(ss, tmp, del)) {
-        if (tmp == " " || tmp.empty()) continue;
+        if (tmp == " " || tmp.empty())
+            continue;
         out.push_back(tmp);
     }
     return out;
 }
 
+template<typename T>
+void fillGlobalDirectives(T &field, T const &value) {
+    if (field != "null")
+        exitError("Error");
+    field = value;
+}
+
+void parseLocation(int directive, std::vector<std::string> const &tokens, std::vector<Server *> &location) {
+
+}
+
+
 int main() {
-    std::vector<Server*> globalConfig;
+    std::vector<Server *> globalConfig;
     std::ifstream ifs;
     const std::string filename("../webserv/config_files/webserv.config");
     ifs.open(filename, std::ios_base::in);
 
     std::string line;
     int i = -1, j = -1;
+    bool isLocation = false;
     if (ifs.is_open()) {
         while (std::getline(ifs, line)) {
-            if (line.empty()) continue;
+            if (line.empty())
+                continue;
+
             std::vector<std::string> tokens = split(line);
             int directive = getDirective(tokens[0]);
             if (tokens[0] == "server")
                 directive = -1;
+            if (isLocation)
+                parseLocation(directive, tokens, globalConfig[i]->_location);
+
             switch (directive) {
                 case -1:
                     globalConfig.push_back(new Server());
                     ++i;
+                    j = -1;
                     break;
-                case eGlobalDirectives::PORT:
-                    if (globalConfig[i]->_port != "null")
-                        exitError("Erooor");
-                    globalConfig[i]->_port = tokens[1];
+                case Directives::PORT:
+                    if (!isLocation)
+                        fillGlobalDirectives(globalConfig[i]->_port, tokens[1]);
+                    else
+                        fillGlobalDirectives(globalConfig[i]->_location[j]->_port, tokens[1]);
                     break;
-                case eGlobalDirectives::HOST:
-                    if (globalConfig[i]->_host != "null")
-                        exitError("Erooor");
-                    globalConfig[i]->_host = tokens[1];
+                case Directives::HOST:
+                    if (!isLocation)
+                        fillGlobalDirectives(globalConfig[i]->_host, tokens[1]);
+                    else
+                        fillGlobalDirectives(globalConfig[i]->_location[j]->_host, tokens[1]);
                     break;
-                case eGlobalDirectives::SERVER_NAME:
-                    if (globalConfig[i]->_server_name != "null")
-                        exitError("Erooor");
-                    globalConfig[i]->_server_name = tokens[1];
+                case Directives::SERVER_NAME:
+                    if (!isLocation)
+                        fillGlobalDirectives(globalConfig[i]->_server_name, tokens[1]);
+                    else
+                        fillGlobalDirectives(globalConfig[i]->_location[j]->_server_name, tokens[1]);
                     break;
-                case eGlobalDirectives::ERROR_PAGE:
-                    if (globalConfig[i]->_error_page != "null")
-                        exitError("Erooor");
-                    globalConfig[i]->_error_page = tokens[1];
+                case Directives::ERROR_PAGE:
+                    if (!isLocation)
+                        fillGlobalDirectives(globalConfig[i]->_error_page, tokens[1]);
+                    else
+                        fillGlobalDirectives(globalConfig[i]->_location[j]->_error_page, tokens[1]);
                     break;
-                case eGlobalDirectives::MAX_FILE_SIZE:
-                    if (globalConfig[i]->_max_file_size != "null")
-                        exitError("Erooor");
-                    globalConfig[i]->_max_file_size = tokens[1];
+                case Directives::MAX_FILE_SIZE:
+                    if (!isLocation)
+                        fillGlobalDirectives(globalConfig[i]->_max_file_size, tokens[1]);
+                    else
+                        fillGlobalDirectives(globalConfig[i]->_location[j]->_max_file_size, tokens[1]);
                     break;
-                case eGlobalDirectives::TIME_OUT:
-                    if (globalConfig[i]->_time_out != "null")
-                        exitError("Erooor");
-                    globalConfig[i]->_time_out = tokens[1];
+                case Directives::TIME_OUT:
+                    if (!isLocation)
+                        fillGlobalDirectives(globalConfig[i]->_time_out, tokens[1]);
+                    else
+                        fillGlobalDirectives(globalConfig[i]->_location[j]->_time_out, tokens[1]);
                     break;
-                case eLocationDirectives::LOC_ROOT:
-                    if (globalConfig[i]->_loc[j]->_root != "null")
-                        exitError("Erooor");
-                    globalConfig[i]->_loc[j]->_root = tokens[1];
+                case Directives::ROOT:
+                    if (!isLocation)
+                        fillGlobalDirectives(globalConfig[i]->_root, tokens[1]);
+                    else
+                        fillGlobalDirectives(globalConfig[i]->_location[j]->_root, tokens[1]);
                     break;
-                case eLocationDirectives::LOC_ALLOWED_METHODS:
-                    if (globalConfig[i]->_loc[j]->_allowed_method != "null")
+                case Directives::ALLOWED_METHODS:
+                    if (!globalConfig[i]->_location[j]->_allowed_method.empty())
                         exitError("Erooor");
-                    globalConfig[i]->_loc[j]->_allowed_method = tokens[1];
+                    if (!isLocation)
+                        for (int k = 1; k < tokens.size(); ++k) globalConfig[i]->_allowed_method.insert(tokens[k]);
+                    else
+                        for (int k = 1; k < tokens.size(); ++k)
+                            globalConfig[i]->_location[j]->_allowed_method.insert(tokens[k]);
                     break;
-                case eLocationDirectives::LOC_INDEX:
-                    if (globalConfig[i]->_loc[j]->_index != "null")
-                        exitError("Erooor");
-                    globalConfig[i]->_loc[j]->_index = tokens[1];
+                case Directives::INDEX:
+                    if (!isLocation)
+                        std::copy(tokens.begin() + 1, tokens.end(), globalConfig[i]->_index.begin());
+                    else std::copy(tokens.begin() + 1, tokens.end(), globalConfig[i]->_location[j]->_index.begin());
                     break;
-                case eLocationDirectives::LOC_AUTO_INDEX:
-                    if (globalConfig[i]->_loc[j]->_auto_index != "null")
-                        exitError("Erooor");
-                    globalConfig[i]->_loc[j]->_auto_index = tokens[1];
+                case Directives::AUTO_INDEX:
+                    if (!isLocation)
+                        fillGlobalDirectives(globalConfig[i]->_auto_index, tokens[1]);
+                    else
+                        fillGlobalDirectives(globalConfig[i]->_location[j]->_auto_index, tokens[1]);
                     break;
-                case eLocationDirectives::LOC_AUTH_BASIC:
-                    if (globalConfig[i]->_loc[j]->_auth_basic != "null")
-                        exitError("Erooor");
-                    globalConfig[i]->_loc[j]->_auth_basic = tokens[1];
+                case Directives::AUTH_BASIC:
+                    if (!isLocation)
+                        fillGlobalDirectives(globalConfig[i]->_auth_basic, tokens[1]);
+                    else
+                        fillGlobalDirectives(globalConfig[i]->_location[j]->_auth_basic, tokens[1]);
                     break;
 
-                case eGlobalDirectives::LOCATION:
-                    globalConfig[i]->_loc.push_back(new Location());
+                case Directives::LOCATION:
+                    globalConfig[i]->_location.push_back(new Server());
                     ++j;
+                    isLocation = true;
                     break;
-//                    if (tokens.size() == 3) globalConfig[tokens[0]] = tokens[1];
-//                    else if (tokens.size() == 4) std::cout << "size is 3" << std::endl;
-//                    else exitError("SYNTAX ERROR.");
+                    //                    if (tokens.size() == 3) globalConfig[tokens[0]] = tokens[1];
+                    //                    else if (tokens.size() == 4) std::cout << "size is 3" << std::endl;
+                    //                    else exitError("SYNTAX ERROR.");
 
-//                case eGlobalDirectives::INVALID:
-//                    exitError("SYNTAX ERROR.");
+                    //                case Directives::INVALID:
+                    //                    exitError("SYNTAX ERROR.");
+                case Directives::LOCATION_END:
+                    isLocation = false;
+                    break;
                 default:
                     break;
                     // exitError("Invalid directive found in config file");
             }
-//            std::cout << tokens << std::endl;
+            //            std::cout << tokens << std::endl;
         }
     } else
         exitError("wrong config file path");
     std::cout << globalConfig << std::endl;
     return EXIT_SUCCESS;
 }
+
+
+
