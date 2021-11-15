378a320489e2c5842601f04dac4cd724125e39d7
Mouad Bani <mbani@e2r8p2.1337.ma>
2021-11-15 15:48:06 +0100

    update request class in main

--- includes/request.hpp
+++ includes/request.hpp
@@ -2,20 +2,33 @@
 // Created by Mohammad  Boustta on 11/12/21.
 //
 
-#ifndef __REQUEST_HPP__
-#define __REQUEST_HPP__
+#ifndef __Request_HPP__
+#define __Request_HPP__
 
 #include <map>
 #include <string>
 #include <vector>
 #include <sstream>
 
+
 class Request {
 private:
-    std::map<std::string, std::vector<std::string> > _requestMap;
+    typedef enum transfer_type
+    {
+        CHUNKED, COMPLETED
+    } transfer_type;
+
+    std::map<std::string, std::vector<std::string> > _RequestMap;
+    std::string		_req;
+    long long 		_size;
+    long long 		_content_length;
+    long long		_header_length;
+    long long		_max_body_size;
+    transfer_type	transfer_encoding;
 public:
 
     Request();
+    Request(long long max_size = 100000);
     ~Request();
     Request(Request const& x);
     Request& operator=(Request const &x);
@@ -24,7 +37,19 @@ public:
 
     std::vector<std::string> const &getValue(std::string const & key);
 
-    void parseRequest(std::stringstream & requestStream);
+    void parseRequest(std::stringstream & RequestStream);
+
+    bool is_completed() const;
+
+    void append(char *content, long long size);
+
+    std::string get_req()const;
+
+    void getReqInfo(std::string );
+
+    long long getContentLength(std::string str);
+
+    size_t getHeaderLength(std::string str);
 
 private:
 
@@ -38,4 +63,4 @@ private:
     bool _isBodyStart(const std::string &line, bool is_body) const;
 };
 
-#endif // __REQUEST_HPP__
+#endif // __Request_HPP__
