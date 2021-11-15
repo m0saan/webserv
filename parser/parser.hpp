d7ce682f9dd848515175f6d0ca5da2b5b500be3e
__mo_san__ <50895527+M0-san@users.noreply.github.com>
2021-11-15 15:29:38 +0100

    Update parser.hpp

--- parser/parser.hpp
+++ parser/parser.hpp
@@ -5,7 +5,10 @@
 #ifndef __PARSER_HPP__
 #define __PARSER_HPP__
 
-class eGlobalDirectives {
+#include <set>
+#include "status_code.h"
+
+class Directives {
 public:
         static const int PORT = 0;
         static const int HOST = 1;
@@ -14,17 +17,53 @@ public:
         static const int MAX_FILE_SIZE = 4;
         static const int TIME_OUT = 5;
         static const int LOCATION = 6;
-        static const int INVALID = 7;
+        static const int REDIRECT = 7;
+        static const int CGI = 8;
+        static const int  ROOT = 9;
+        static const int  ALLOWED_METHODS = 10;
+        static const int  INDEX = 11;
+        static const int  AUTO_INDEX = 12;
+        static const int  AUTH_BASIC = 13;
+        static const int  UPLOAD = 14;
+        static const int  LOCATION_END = 99;
+        static const int  SERVER_END = 100;
+
 };
 
-class eLocationDirectives : public eGlobalDirectives
-{
-public:
-    static const int  LOC_ROOT = 9;
-    static const int  LOC_ALLOWED_METHODS = 10;
-    static const int  LOC_INDEX = 11;
-    static const int  LOC_AUTO_INDEX = 12;
-    static const int  LOC_AUTH_BASIC = 13;
+typedef enum error {
+    INVALID_DIRECTIVE = -2,
+}            e_error;
+
+struct Server { 
+    Server() :
+    _port("null"), _host("null"), _server_name("null"),
+    _error_page("null"),_max_file_size("null"),
+    _time_out("null"), _cgi("null"), _root("null"),
+    _auto_index("null"), _auth_basic("null"), _redirect(), _index(),
+    _allowed_method(), _location(), upload_pass() {}
+
+    std::string _port;
+    std::string _host;
+    std::string _server_name;
+    std::string _error_page;
+    std::string _max_file_size;
+    std::string _time_out;
+    std::string _cgi;
+    std::string _root;
+    std::string _auto_index;
+    std::string _auth_basic;
+
+    std::pair<HTTP::StatusCodes, std::string> _redirect;
+    std::vector<std::string> _index;
+    std::set<std::string> _allowed_method;
+
+    std::vector<Server*> _location;
+
+    bool upload_pass;
+
+
 };
+
+static void foo();
 
 #endif // __PARSER_HPP__
