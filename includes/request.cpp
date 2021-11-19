542a86292ca1fed97c5346751fe4089753a487ec
mo-san99 <moboustta6@gmail.com>
2021-11-20 00:14:30 +0100

    support chucked body for the HTTP request

--- includes/request.cpp
+++ includes/request.cpp
@@ -8,82 +8,110 @@
 
 Request::Request() {}
 
-Request::Request(long long max_size ): _size(-1), _content_length(-1), _header_length(-1), _max_body_size(max_size) { }
+Request::Request(long long max_size) : _size(-1), _content_length(-1), _header_length(-1), _max_body_size(max_size) {}
 
 Request::~Request() {}
 
-Request::Request(const Request &x) {
-    *this = x;
+Request::Request(const Request &x)
+{
+	*this = x;
 }
 
-Request &Request::operator=(const Request &x) {
-//    this->_RequestMap = x._RequestMap;
-    return *this;
+Request &Request::operator=(const Request &x)
+{
+	//    this->_RequestMap = x._RequestMap;
+	return *this;
 }
 
-std::vector<std::string> const &Request::getValue(const std::string &key) {
-    return _RequestMap[key];
+std::vector<std::string> const &Request::getValue(const std::string &key)
+{
+	return _RequestMap[key];
 }
 
-void Request::parseRequest(std::stringstream &RequestStream) {
-    std::string line;
-    bool is_body(false);
-    while (std::getline(RequestStream, line)) {
+void Request::parseRequest(std::stringstream &RequestStream)
+{
+	std::string line;
+	bool is_body(false);
+	bool is_chunked(false);
+	while (std::getline(RequestStream, line))
+	{
+		if (line.empty() || _isBodyStart(line, is_body))
+		{
+			is_body = true;
+			continue;
+		}
+		if (_isBodyEnd(line))
+		{
+			is_body = false;
+			continue;
+		};
 
-        if (line.empty()) {
-            is_body = true;
-            continue;
-        }
-        if (_isBodyEnd(line)) {
-            is_body = false;
-            continue;
-        };
-        if (_isBodyStart(line, is_body)) {
-            is_body = true;
-            continue;
-        }
-        if (!is_body) _getHeader(line);
-        else _getBody(line);
-    }
+		if (_isChunckStart(line))
+		{
+			is_chunked = true;
+			continue;
+		}
+
+		if (!is_body)
+			_getHeader(line);
+		else
+			_getBody(line, is_chunked);
+	}
+}
+
+
+bool Request::_isChunckStart(std::string const &line) const
+{
+	return (line.length() > 0 && isnumber(line[0]));
 }
 
 bool Request::_isBodyStart(const std::string &line, bool is_body) const { return is_body && line == "{"; }
 
 bool Request::_isBodyEnd(const std::string &line) const { return line == "}"; }
 
-void Request::_getBody(std::string &line) {
-    line.push_back('\n');
-    if (_RequestMap.count("Body:"))
-        _RequestMap["Body:"][0] += line;
-    else
-        _RequestMap["Body:"].push_back(line);
+void Request::_getBody(std::string &line, bool is_chunked)
+{
+	line.push_back('\n');
+	static int i = 0;
+	if (is_chunked)
+		if (i % 2 != 0)
+			return;
+	if (_RequestMap.count("body"))
+		_RequestMap["body"][0] += line;
+	else
+		_RequestMap["body"].push_back(line);
 }
 
 bool Request::_isBody(std::string const &line, bool const &is_body) const { return line == "{" && is_body; }
 
-void Request::_getHeader(const std::string &line) {
-    std::__1::vector<std::string> tokens = Utility::split(line, ' ');
-    if (tokens[0] == "GET" || tokens[0] == "POST" || tokens[0] == "DELETE")
-        _RequestMap["SL"] = std::__1::vector<std::string>(tokens.begin(), tokens.end());
-    else
-        _RequestMap[tokens[0]] = std::__1::vector<std::string>(tokens.begin() + 1, tokens.end());
+void Request::_getHeader(const std::string &line)
+{
+	std::__1::vector<std::string> tokens = Utility::split(line, ' ');
+	if (tokens[0] == "GET" || tokens[0] == "POST" || tokens[0] == "DELETE")
+		_RequestMap["SL"] = std::__1::vector<std::string>(tokens.begin(), tokens.end());
+	else
+	{
+		tokens[0].pop_back();
+		_RequestMap[tokens[0]] = std::__1::vector<std::string>(tokens.begin() + 1, tokens.end());
+	}
 }
 
-std::map<std::string, std::vector<std::string> > const &Request::getMap() const {
-    return _RequestMap;
+std::map<std::string, std::vector<std::string> > const &Request::getMap() const
+{
+	return _RequestMap;
 }
 
-std::string Request::get_req()const
+std::string Request::get_req() const
 {
 	return this->_req;
 }
 
-bool	Request::is_completed() const
+bool Request::is_completed() const
 {
 	return (_size == _content_length + _header_length);
 }
 
-void 	Request::append(char *content, long long size)
+void Request::append(char *content, long long size)
 {
 	std::string tmp(content, size);
 	if (_content_length == -1) // 1st time reading req
@@ -131,6 +159,6 @@ long long Request::getContentLength(std::string str)
 			else maybe it's a chunked req or bad Request
 
 	*/
-	
+
 	return length;
 }
