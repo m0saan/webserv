0d106b9c662e7dc1720621b6df697c9190ed072f
mo-san99 <moboustta6@gmail.com>
2021-12-04 22:03:00 +0100

    add main

--- main.cpp
+++ main.cpp
@@ -5,20 +5,26 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <regex>
+
 #include "parser/parser.hpp"
 #include "includes/request.hpp"
 #include "includes/utility.hpp"
 #include "includes/server.hpp"
 
+std::string getScriptName(std::string const& url) {
+    std::size_t 
+}
 
-int main(int ac, char **av) {
+int main(int ac, char **av)
+{
     if (ac != 2)
         exit(1);
     Request request;
     std::ifstream ifs;
 
     // std::cout << performParsing().size() << std::endl;
-    Server serv(performParsing(av[1]));
+    // Server serv(performParsing(av[1]));
     // ifs.open("./request", std::ios_base::in);
     // if (ifs.is_open()) {
     //     std::stringstream ss;
@@ -36,9 +42,22 @@ int main(int ac, char **av) {
     //         std::cout << std::endl;
     //     }
     // }
-    serv.listen();
+    // serv.listen();
+    char value_store[100];
+    memset(value_store, 0, 100);
+    std::string url("http://login:password@example.com/one/more/dir/file.exe?a=sth&b=sth");
+    sscanf(url.c_str(), "%#*/", value_store);
+    std::cout << value_store << std::endl;
+    // std::map<std::string, std::string> queries;
+    // std::pair<bool, int> has_queries_result;
+    // has_queries_result = hasQueries(url);
+    // if (has_queries_result.first)
+    //     queries = getQueries(url, has_queries_result.second);
+
+    // for (auto const p : queries)
+    //     std::cout << p.first << ":" << p.second << std::endl;
+
+
 
     return EXIT_SUCCESS;
 }
-
-
